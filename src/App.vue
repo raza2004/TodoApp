ac161b86ac65762b38b5e710cee980b6e37e6380
raza2004 <owaisraza3274@gmail.com>
2023-01-22 21:01:59 +0500

    s

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+ <TodoList/>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    
+    TodoList
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: justify;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-color: #bed0e2;
+  color: rgb(6, 59, 41);
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="TodoList">
+    <form @submit.prevent="addTodo">
+      <input class="todo-text" type="text" v-model="newTodo" placeholder="Add a new to-do" />
+      <button type="submit">Add</button>
+    </form>
+    <ul class="ul">
+      <li v-for="(todo, index) in todos" :key="todo.id" 
+      
+      >
+        {{ todo.text }}
+        <button class="remove" @click="removeTodo(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      newTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: Date.now(),
+        text: this.newTodo
+      });
+      this.newTodo = "";
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+};
+</script>
+<style scoped>
+.todo-item {
+    display: flex;
+    align-items: center;
+    padding: 0.5em;
+    /* background-color: #f4f4f4; */
+    margin-bottom: 0.5em;
+    
+}
+
+.todo-text {
+    flex-grow: 1;
+    margin-right: 0em;
+}
+
+.remove{
+    background-color: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 0.5em;
+    cursor: pointer;
+}
+.ul{
+  padding: 2em;
+}
+</style>
+
